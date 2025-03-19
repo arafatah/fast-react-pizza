0fe3f66a69357190a688d2004293b8e0e61cbdc1
arafatah <arafathowlader890@gmail.com>
2025-03-19 14:39:04 +0600

    Add Header and AppLayout components; update routing and ESLint configuration

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+    return (
+        <div>
+            <Link to="/">Fast React pizza co.</Link>
+            <p>Arafat Hung </p>
+        </div>
+    )
+}
+
+export default Header
