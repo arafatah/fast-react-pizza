40a676dc897cba0ea76b214d3f79f034def613ae
arafatah <arafathowlader890@gmail.com>
2025-03-20 11:46:41 +0600

    Refactor cart components and enhance menu display; add loading state and Loader component

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,14 +1,22 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import Header from "./Header";
+import CartOverview from "../features/cart/CartOverview";
+import Loader from "./Loader";
 
 function AppLayout() {
-  return (
-    <div>
-      <Header />
+  const navigation = useNavigation();
+  const isLoading = navigation.state === "loading";
 
+  return (
+    <div className="layout">
+      {isLoading && <Loader />}
+
+      <Header />
       <main>
         <Outlet />
       </main>
+
+      <CartOverview />
     </div>
   );
 }
