9a41631a213d8f226f4f2c0c85cdd55a6acf5270
arafatah <arafathowlader890@gmail.com>
2025-03-20 12:57:24 +0600

    Add error handling to routing; implement SearchOrder component in Header

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
-    return (
-        <div>
-            <Link to="/">Fast React pizza co.</Link>
-            <p>Arafat Hung </p>
-        </div>
-    )
+  return (
+    <div>
+      <Link to="/">Fast React pizza co.</Link>
+
+      <SearchOrder />
+
+      <p>Arafat Hung </p>
+    </div>
+  );
 }
 
-export default Header
+export default Header;
