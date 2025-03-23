0b792fff7a7910d9f755a1aee1e4bc8e17794ffb
arafatah <arafathowlader890@gmail.com>
2025-03-23 13:03:47 +0600

    Refactor CartOverview, Username, Header, and Home components; enhance styling and layout

--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Arafat</div>;
+  return <div className="text-sm font-semibold hidden">Arafat</div>;
 }
 
 export default Username;
