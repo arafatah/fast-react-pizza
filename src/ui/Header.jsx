879ee3569017bdf2cb4b1e51389627325ddbd0b4
arafatah <arafathowlader890@gmail.com>
2025-03-23 12:56:49 +0600

    Add Username component and integrate into Header; include CreateUser component in Home

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
 import SearchOrder from "../features/order/SearchOrder";
+import Username from "../features/user/Username";
 
 function Header() {
   return (
-  <div className="bg-yellow-500 uppercase" >
-      <Link to="/" className="tracking-widest">Fast React pizza co.</Link>
+    <div className="bg-yellow-500 uppercase">
+      <Link to="/" className="tracking-widest">
+        Fast React pizza co.
+      </Link>
 
       <SearchOrder />
 
-      <p>Arafat Hung </p>
+      <Username />
     </div>
   );
 }
