97c7f1b45153565477d126145ae9748e124db0da
MD. Arafat Howlader <47807293+arafatah@users.noreply.github.com>
2025-03-30 05:29:35 +0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
